c45a0d61bf58a89c14f71b91cf9da5430a71da30
Ígor Yamamoto <igor.a.r.y@gmail.com>
2017-07-28 14:54:32 -0300

    Update Skills section

--- js/app.js
+++ js/app.js
@@ -10,8 +10,8 @@ function showBlocks(blocks, offset) {
   });
 }
 function hello() {
-    $('#hello-txt').removeClass().addClass('pulse animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-      $('#hello-txt').removeClass();
+    $(this).removeClass().addClass('pulse animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+      $(this).removeClass();
     });
 };
 function gogo() {
@@ -22,16 +22,87 @@ function gogo() {
       $.fn.fullpage.moveSectionDown();
     }, 1800);
 };
+function skillSelect(className) {
+  $('.skill-group').addClass('text-muted');
+  $(className).removeClass('text-muted');
+  $('.circles-group').css({transform: 'scale(0.5)', display: 'none'});
+  $('.circles-group' + className).css({transform: 'scale(1)', display: 'block'});
+  switch (className) {
+    case '.backend':
+      $('.circles-group.frontend').css({display: 'block'});
+      break;
+    case '.frontend':
+      $('.circles-group.engineering').css({display: 'block'});
+      break;
+    case '.engineering':
+      $('.circles-group.others').css({display: 'block'});
+      break;
+    case '.others':
+      $('.circles-group.lang').css({display: 'block'});
+      break;
+    case '.lang':
+      $('.circles-group.continue').css({display: 'block'});
+      break;
+    default:
+  }
+};
+function createCircles() {
+    var skills = [
+      { id: 'python', colors: ['#f0f0f0', '#407EB0'], value: 90, text: 'Python' },
+      { id: 'ruby', colors: ['#f0f0f0', '#EC1622'], value: 50, text: 'Ruby (Rails)' },
+      { id: 'sql', colors: ['#f0f0f0', '#1BAFEC'], value: 65, text: 'SQL' },
+      { id: 'html', colors: ['#f0f0f0', '#E54F27'], value: 95, text: 'HTML5' },
+      { id: 'css', colors: ['#f0f0f0', '#2177FF'], value: 90, text: 'CSS3' },
+      { id: 'js', colors: ['#f0f0f0', '#F0DB4F'], value: 70, text: 'Javascript' },
+      { id: 'matlab', colors: ['#f0f0f0', '#FF8911'], value: 75, text: 'MATLAB' },
+      { id: 'labview', colors: ['#f0f0f0', '#FFE700'], value: 60, text: 'LabVIEW' },
+      { id: 'arduino', colors: ['#f0f0f0', '#0CA1A6'], value: 75, text: 'Arduino' },
+      { id: 'git', colors: ['#f0f0f0', '#24292E'], value: 85, text: 'Git' },
+      { id: 'latex', colors: ['#f0f0f0', '#008080'], value: 65, text: 'Latex' },
+      { id: 'learn', colors: ['#f0f0f0', '#0E579C'], value: 95, text: 'Learn new tech' },
+      { id: 'pt', colors: ['#f0f0f0', '#239E46'], value: 99.9, text: 'Portuguese' },
+      { id: 'en', colors: ['#f0f0f0', '#EC1C24'], value: 75, text: 'English' },
+    ];
+		skills.map(function(s) {
+			Circles.create({
+				id: s.id,
+				value: s.value,
+				radius: 80,
+				width: 20,
+				colors: s.colors,
+        text: s.text,
+        textClass: 'skill-txt',
+        styleText: true,
+			});
+    });
+}
 $(document).ready(function() {
   var timelineBlocks = $('.cd-timeline-block'),
       offset = 0.8,
-      secColor = '#f5f5f7';
+      secColor = '#f5f5f7',
+      currSkillGroup = '.backend';
+  skillSelect(currSkillGroup);
   if ($(window).width() <= 1024) {
     $('nav').hide();
     $('#logo').css('padding','0px');
   } else {
     $('nav').fadeIn();
   };
+  $('#hello-txt').click(hello);
+// Skills
+  $('.skill-group').hover(
+    function() {
+      var className = '.' + $(this).attr('class').split(' ')[0];
+      skillSelect(className);
+    },
+    function() {
+      skillSelect(currSkillGroup);
+    }
+  ).click(
+    function() {
+      currSkillGroup = '.' + $(this).attr('class').split(' ')[0];
+    }
+  );
   $(window).on('resize', function() {
     if ($(window).width() <= 1024) {
       $('nav').hide();
@@ -93,6 +164,13 @@ $(document).ready(function() {
     paddingTop: '70px',
     bigSectionsDestination: 'top',
     fitToSection: false,
+    onLeave(index, nextIndex, direction) {
+      if(nextIndex == 3) {
+        window.setTimeout(function() {
+          createCircles();
+        }, 400);
+      }
+    },
   });
 });
 // Close dropdown when click a section on mobile
